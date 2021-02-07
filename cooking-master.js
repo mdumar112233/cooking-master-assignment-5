b7d2cdbe6c4f68300fc8a72864402fc1090fd4a6
mduumar112233 <mdumarfaruk187326@gmail.com>
2021-02-07 22:35:20 +0600

    added new function

--- cooking-master.js
+++ cooking-master.js
@@ -1,6 +1,6 @@
+// display all result of search value
 const getItemResult = allItem => {
     const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${allItem}`;
-    // const url = `https://www.themealdb.com/api/json/v1/1/filter.php?i=${allItem}`;
     fetch(url)
     .then(res => res.json())
     .then(data => findItem(data))
@@ -23,9 +23,9 @@ const findItem = displayItem => {
         let item = '';
         displayItem.meals.forEach(meal => {
              item += `
-            <div class='item'>    
+            <div id='change' class='item'>    
             <div class='meal-img'>
-                <img  src='${meal.strMealThumb}'>
+                <img onclick='displayDetails("${meal.strMeal}")'  src='${meal.strMealThumb}'>
             </div>
             <div class='meal-name'>
             <h3>${meal.strMeal}</h3>
@@ -45,11 +45,61 @@ const findItem = displayItem => {
             document.getElementById('warning-btn').style.display = 'block';
         }
     }
-
 }
 
+// display any single item details with ingredient
+const displayDetails = details => {
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${details}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => singleItemDetails(data.meals[0]))
+}
 
-    // getItemResult()
+const singleItemDetails = singleItem =>{
+    const mealDetails = document.getElementById('meal-details');
+        const item = `
+        <div id='change' class='item-single'>    
+        <div class='meal-img'>
+            <img  src='${singleItem.strMealThumb}'>
+        </div>
+        <div class='meal-name-single'>
+        <h3>${singleItem.strMeal}</h3>
+        </div>
+        <h3 class='ing-title'>Ingredient</h3>
+        <div id='ingredient'>
+        <p>${singleItem.strIngredient1}</p>
+        <p>${singleItem.strIngredient2}</p>
+        <p>${singleItem.strIngredient3}</p>
+        <p>${singleItem.strIngredient4}</p>
+        <p>${singleItem.strIngredient5}</p>
+        <p>${singleItem.strIngredient6}</p>
+        <p>${singleItem.strIngredient7}</p>
+        <p>${singleItem.strIngredient8}</p>
+        <p>${singleItem.strIngredient9}</p>
+        <p>${singleItem.strIngredient10}</p>
+        </div>
+        `
+
+        mealDetails.innerHTML = item;
+        document.getElementById('meal-item').style.display = 'none';
+}
+
+document.getElementById('input-item').value = '';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
